44169afe6f3cd87f462dc50242d2d180fdcc73f7
Fabrício <fabricio@pinaculus.com.br>
2018-04-17 13:37:03 -0300

    Modificações de layout e ajustes

--- src/app/timecard/timecard-list/timecard-list.component.ts
+++ src/app/timecard/timecard-list/timecard-list.component.ts
@@ -214,7 +214,7 @@ export class TimecardListComponent implements OnInit {
       })
       this.renewStatus()
       return;
-    }, {timeout: 5000})
+    }, {timeout: 5000, enableHighAccuracy: true})
   }
 
   total(timecards: Timecard[]): string {
