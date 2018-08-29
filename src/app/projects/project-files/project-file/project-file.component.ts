83021f493f548be130cd5520f447960e61bd1043
Weder Fabrício <weder.fabricio@hotmail.com>
2018-08-29 16:55:39 -0300

    Imagens com bordas na aba projeto.

---
+++ src/app/projects/project-files/project-file/project-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cb-project-file',
+  templateUrl: './project-file.component.html',
+  styleUrls: ['./project-file.component.css']
+})
+export class ProjectFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
