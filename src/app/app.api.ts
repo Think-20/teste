0169df09e5eef623ad42d51dde8d3d0cc11a1414
Weder Fabrício <weder.fabricio@hotmail.com>
2018-10-17 16:50:19 -0300

    Compilação dos arquivos.

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,5 +1,5 @@
 import { environment } from '../environments/environment'
 
 export const API = environment.api
-export const FRONTEND_VERSION = '1.15.39'
-export const BACKEND_VERSION = '1.15.32'
+export const FRONTEND_VERSION = '1.15.40'
+export const BACKEND_VERSION = '1.15.33'
