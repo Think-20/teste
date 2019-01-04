4db71b1ed66eb7ce7d1a3de60fd4e2466744e6ef
Weder Fabrício <weder.fabricio@hotmail.com>
2019-01-04 14:09:43 -0200

    Modificação do estilo do checkbox do memorial.

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,5 +1,5 @@
 import { environment } from '../environments/environment'
 
 export const API = environment.api
-export const FRONTEND_VERSION = '1.16.13'
+export const FRONTEND_VERSION = '1.16.14'
 export const BACKEND_VERSION = '1.16.8'
