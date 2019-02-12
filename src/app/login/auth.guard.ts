e9738b6ddc85bdb74d978acf2de55759ed0afe75
Weder Fabrício <weder.fabricio@hotmail.com>
2019-02-12 10:45:06 -0200

    Ajuste para sessão expirada quando o token estiver vazio.

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -3,10 +3,10 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { MatSnackBar } from '@angular/material';
 
 import { AuthService } from './auth.service';
- 
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-    
+
     constructor(
         private router: Router,
         private auth: AuthService,
@@ -21,15 +21,15 @@ export class AuthGuard implements CanActivate {
         let user = this.auth.currentUser()
         let originalRoute = '/' + iterativeRoute.routeConfig.path
 
-        while(iterativeRoute.parent !== null 
-            && iterativeRoute.parent.routeConfig !== null 
-            && iterativeRoute.parent.routeConfig.path != '') 
+        while(iterativeRoute.parent !== null
+            && iterativeRoute.parent.routeConfig !== null
+            && iterativeRoute.parent.routeConfig.path != '')
         {
             iterativeRoute = iterativeRoute.parent
             originalRoute = '/' + iterativeRoute.routeConfig.path + originalRoute
         }
 
-        if(user === null) {
+        if(user === null || this.auth.token() == '' || this.auth.token() == null) {
             this.snackBar.open('Desculpe, sua sessão expirou.', '', { duration: 3000 })
             this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
             return false;
@@ -38,7 +38,7 @@ export class AuthGuard implements CanActivate {
         let foundDisplays = user.displays.filter(display => {
             return display.url === originalRoute && display.access === 'N'
         })
-        
+
         if(foundDisplays.length > 0) {
             this.snackBar.open('Desculpe, você não tem permissão de acesso para essa funcionalidade.', '', { duration: 3000 })
             return false;
