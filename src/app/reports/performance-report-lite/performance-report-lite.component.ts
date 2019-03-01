cd24afbcb9598e962cca50a87df48d2150237367
Weder Fabrício <weder.fabricio@hotmail.com>
2019-03-01 14:47:31 -0300

    Início da construção da tela para parametrização e visualização dos dados.

--- src/app/reports/performance-report-lite/performance-report-lite.component.ts
+++ src/app/reports/performance-report-lite/performance-report-lite.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Employee } from '../../employees/employee.model';
+import { EmployeeService } from '../../employees/employee.service';
 
 @Component({
   selector: 'cb-performance-report-lite',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerformanceReportLiteComponent implements OnInit {
 
-  constructor() { }
+  filterOpen: boolean = true
+  searchForm: FormGroup
+  attendances: Employee[]
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private employeeService: EmployeeService
+  ) { }
 
   ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      initial_date: this.formBuilder.control('', [
+        Validators.required
+      ]),
+      final_date: this.formBuilder.control('', [
+        Validators.required
+      ]),
+      time_to_analyze: this.formBuilder.control('', [
+        Validators.required
+      ]),
+      attendances: this.formBuilder.control('')
+    })
+
+    this.loadData()
   }
 
+  loadData() {
+    this.employeeService.employees().subscribe((dataInfo) => {
+      let employees = <Employee[]> dataInfo.pagination.data
+      this.attendances = employees.filter((employee) => employee.department.description == 'Atendimento')
+    })
+  }
+
+  compareAttendance(var1: Employee, var2: Employee) {
+    return var1.id === var2.id
+  }
 }
