eb207786ba59b784acff4560782803fa5835d2b4
Weder Fabrício <weder.fabricio@hotmail.com>
2019-07-11 10:03:05 -0300

    Mudar display-list para padrão ListData
    Alteração para unificação em componente ListData

--- src/app/displays/display-list/display-list.component.ts
+++ src/app/displays/display-list/display-list.component.ts
@@ -1,64 +1,24 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
 import { DisplayService } from '../display.service';
 import { Display } from '../display.model';
 import { AuthService } from '../../login/auth.service';
-import { Pagination } from '../../shared/pagination.model';
-import { DataInfo } from '../../shared/data-info.model';
-import { distinctUntilChanged } from 'rxjs/operators';
-import { debounceTime } from 'rxjs/operator/debounceTime';
-import { API } from '../../app.api';
+import { ListData, mF } from 'app/shared/list-data/list-data.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cb-display-list',
-  templateUrl: './display-list.component.html',
-  styleUrls: ['./display-list.component.css'],
-  animations: [
-    trigger('rowAppeared', [
-      state('ready', style({opacity: 1})),
-      transition('void => ready', animate('300ms 0s ease-in', keyframes([
-        style({opacity: 0, transform: 'translateX(-30px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(10px)', offset: 0.8}),
-        style({opacity: 1, transform: 'translateX(0px)', offset: 1})
-      ]))),
-      transition('ready => void', animate('300ms 0s ease-out', keyframes([
-        style({opacity: 1, transform: 'translateX(0px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2}),
-        style({opacity: 0, transform: 'translateX(30px)', offset: 1})
-      ])))
-    ])
-  ]
+  templateUrl: './display-list.component.html'
 })
 @Injectable()
 export class DisplayListComponent implements OnInit {
-
-  rowAppearedState: string = 'ready'
-  searchForm: FormGroup
-  formCopy: any
-  search: FormControl
-  displays: Display[] = []
-  searching = false
-  filter: boolean = false
-  pagination: Pagination
-  pageIndex: number
-  params = {}
-  hasFilterActive = false
-  dataInfo: DataInfo
-  API = API
-
+  listData: ListData;
   constructor(
-    private fb: FormBuilder,
     private displayService: DisplayService,
     private authService: AuthService,
+    private router: Router,
     private snackBar: MatSnackBar
   ) { }
-
-  total(displays: Display[]) {
-    return displays.length
-  }
 
   permissionVerify(module: string, display: Display): boolean {
     let access: boolean
@@ -84,110 +44,101 @@ export class DisplayListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.pageIndex = this.displayService.pageIndex
-
-    this.createForm()
-    this.loadInitialData()
-  }
-
-  createForm() {
-    this.search = this.fb.control('')
-    this.searchForm = this.fb.group({
-      search: this.search,
-    })
-
-    this.formCopy = this.searchForm.value
-
-    if(JSON.stringify(this.displayService.searchValue) == JSON.stringify({})) {
-      this.displayService.searchValue = this.searchForm.value
-    } else {
-      this.searchForm.setValue(this.displayService.searchValue)
-    }
-
-    this.searchForm.valueChanges
-    .pipe(distinctUntilChanged())
-    .debounceTime(500)
-    .subscribe((searchValue) => {
-      this.params = this.getParams(searchValue)
-      this.loadDisplays(this.params, 1)
-
-      this.pageIndex = 0
-      this.displayService.pageIndex = 0
-      this.displayService.searchValue = searchValue
-      this.updateFilterActive()
-    })
-  }
-
-  getParams(searchValue) {
-    return {
-      search: searchValue.search,
-    }
-  }
-
-  updateFilterActive() {
-    if (JSON.stringify(this.displayService.searchValue) === JSON.stringify(this.formCopy)) {
-      this.hasFilterActive = false
-    } else {
-      this.hasFilterActive = true
-    }
-  }
-
-  clearFilter() {
-    this.displayService.searchValue = {}
-    this.displayService.pageIndex = 0
-    this.pageIndex = 0
-    this.createForm()
-    this.loadInitialData()
-  }
-
-  loadInitialData() {
-    if (JSON.stringify(this.displayService.searchValue) === JSON.stringify(this.formCopy)) {
-      this.loadDisplays({}, this.pageIndex + 1)
-    } else {
-      this.params = this.getParams(this.displayService.searchValue)
-      this.loadDisplays(this.params, this.displayService.pageIndex + 1)
-    }
-
-    this.updateFilterActive()
-  }
-
-  loadDisplays(params, page: number) {
-    this.searching = true
-    let snackBar = this.snackBar.open('Carregando dados...')
-
-    this.displayService.displays(params, page).subscribe(dataInfo => {
-      this.searching = false
-      this.dataInfo = dataInfo
-      this.pagination = dataInfo.pagination
-      this.displays = <Display[]> this.pagination.data
-      snackBar.dismiss()
-    })
-  }
-
-  delete(display: Display) {
-    this.displayService.delete(display.id).subscribe((data) => {
-      this.snackBar.open(data.message, '', {
-        duration: 5000
-      })
-
-      if(data.status) {
-        this.displays.splice(this.displays.indexOf(display), 1)
+    this.listData = {
+      header: {
+        filterFields: [
+          mF({
+            class: 'col-md-3',
+            formcontrolname: 'search',
+            placeholder: 'Descrição',
+            type: 'input'
+          }),
+        ],
+        getParams: (formValue) => {
+          return {
+            search: formValue.search
+          }
+        }
+      },
+      body: {
+        dataFields: [
+          {
+            label: 'ID',
+            style: { width: '12%' },
+            showData: (display: Display) => {
+              return display.id
+            }
+          },
+          {
+            label: 'Descrição',
+            style: { width: '30%' },
+            showData: (display: Display) => {
+              return display.description
+            }
+          },
+          {
+            label: 'URL',
+            style: { width: '50%' },
+            showData: (display: Display) => {
+              return display.url
+            }
+          },
+        ],
+        hasMenuButton: true,
+        menuItems: [
+          {
+            icon: 'subject',
+            label: 'Detalhes',
+            actions: {
+              click: (display: Display) => {
+                return this.router.navigate(['/cost-categories/show', display.id])
+              },
+              disabled: (display: Display) => {
+                return !this.permissionVerify('show', display)
+              }
+            }
+          },
+          {
+            icon: 'mode_edit',
+            label: 'Editar',
+            actions: {
+              click: (display: Display) => {
+                return this.router.navigate(['/cost-categories/edit', display.id])
+              },
+              disabled: (display: Display) => {
+                return !this.permissionVerify('edit', display)
+              }
+            }
+          },
+          {
+            icon: 'delete',
+            label: 'Remover',
+            removeWhenClickTrue: true,
+            actions: {
+              click: (display: Display) => {
+                return this.delete(display)
+              },
+              disabled: (display: Display) => {
+                return !this.permissionVerify('delete', display)
+              }
+            }
+          },
+        ],
+        loadData: (params, page) => {
+          return this.displayService.displays(params, page)
+        },
+        buttonStyle: { width: '8%' }
       }
-    })
+    }
   }
 
-  changePage($event) {
-    this.searching = true
-    this.displays = []
+  async delete(display: Display) {
+    let data = await this.displayService.delete(display.id).toPromise()
 
-    this.displayService.displays(this.displayService.searchValue, ($event.pageIndex + 1)).subscribe(dataInfo => {
-      this.searching = false
-      this.dataInfo = dataInfo
-      this.pagination = dataInfo.pagination
-      this.displays = <Display[]> this.pagination.data
-      this.pageIndex = $event.pageIndex
-      this.displayService.pageIndex = this.pageIndex
+    this.snackBar.open(data.message, '', {
+      duration: 5000
     })
-  }
 
+    return data.status
+  }
 }
