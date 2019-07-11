3c322e052db883b60acc3f5719fb93535974d554
Weder Fabrício <weder.fabricio@hotmail.com>
2019-07-11 10:45:30 -0300

    Mudar functionality-list para padrão ListData
    Alteração para unificação em componente ListData

--- src/app/functionalities/functionality-list/functionality-list.component.ts
+++ src/app/functionalities/functionality-list/functionality-list.component.ts
@@ -1,70 +1,30 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { AuthService } from '../../login/auth.service';
+import { ListData, mF } from 'app/shared/list-data/list-data.model';
+import { Router } from '@angular/router';
 import { FunctionalityService } from '../functionality.service';
 import { Functionality } from '../functionality.model';
-import { AuthService } from '../../login/auth.service';
-import { Pagination } from '../../shared/pagination.model';
-import { DataInfo } from '../../shared/data-info.model';
-import { distinctUntilChanged } from 'rxjs/operators';
-import { debounceTime } from 'rxjs/operator/debounceTime';
-import { API } from '../../app.api';
 
 @Component({
   selector: 'cb-functionality-list',
-  templateUrl: './functionality-list.component.html',
-  styleUrls: ['./functionality-list.component.css'],
-  animations: [
-    trigger('rowAppeared', [
-      state('ready', style({opacity: 1})),
-      transition('void => ready', animate('300ms 0s ease-in', keyframes([
-        style({opacity: 0, transform: 'translateX(-30px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(10px)', offset: 0.8}),
-        style({opacity: 1, transform: 'translateX(0px)', offset: 1})
-      ]))),
-      transition('ready => void', animate('300ms 0s ease-out', keyframes([
-        style({opacity: 1, transform: 'translateX(0px)', offset: 0}),
-        style({opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2}),
-        style({opacity: 0, transform: 'translateX(30px)', offset: 1})
-      ])))
-    ])
-  ]
+  templateUrl: './functionality-list.component.html'
 })
 @Injectable()
 export class FunctionalityListComponent implements OnInit {
-
-  rowAppearedState: string = 'ready'
-  searchForm: FormGroup
-  formCopy: any
-  search: FormControl
-  functionalities: Functionality[] = []
-  searching = false
-  filter: boolean = false
-  pagination: Pagination
-  dataInfo: DataInfo
-  pageIndex: number
-  params = {}
-  hasFilterActive = false
-  API = API
-
+  listData: ListData;
   constructor(
-    private fb: FormBuilder,
     private functionalityService: FunctionalityService,
     private authService: AuthService,
+    private router: Router,
     private snackBar: MatSnackBar
   ) { }
-
-  total(functionalities: Functionality[]) {
-    return functionalities.length
-  }
 
   permissionVerify(module: string, functionality: Functionality): boolean {
     let access: boolean
     switch(module) {
       case 'show': {
-        access = functionality.id != functionality.id ? this.authService.hasAccess('functionalities/get/{id}') : true
+        access = functionality.id != functionality.id ? this.authService.hasAccess('functionalitys/get/{id}') : true
         break
       }
       case 'edit': {
@@ -84,110 +44,101 @@ export class FunctionalityListComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.pageIndex = this.functionalityService.pageIndex
-
-    this.createForm()
-    this.loadInitialData()
-  }
-
-  createForm() {
-    this.search = this.fb.control('')
-    this.searchForm = this.fb.group({
-      search: this.search,
-    })
-
-    this.formCopy = this.searchForm.value
-
-    if(JSON.stringify(this.functionalityService.searchValue) == JSON.stringify({})) {
-      this.functionalityService.searchValue = this.searchForm.value
-    } else {
-      this.searchForm.setValue(this.functionalityService.searchValue)
-    }
-
-    this.searchForm.valueChanges
-    .pipe(distinctUntilChanged())
-    .debounceTime(500)
-    .subscribe((searchValue) => {
-      this.params = this.getParams(searchValue)
-      this.loadFunctionalities(this.params, 1)
-
-      this.pageIndex = 0
-      this.functionalityService.pageIndex = 0
-      this.functionalityService.searchValue = searchValue
-      this.updateFilterActive()
-    })
-  }
-
-  getParams(searchValue) {
-    return {
-      search: searchValue.search,
-    }
-  }
-
-  updateFilterActive() {
-    if (JSON.stringify(this.functionalityService.searchValue) === JSON.stringify(this.formCopy)) {
-      this.hasFilterActive = false
-    } else {
-      this.hasFilterActive = true
-    }
-  }
-
-  clearFilter() {
-    this.functionalityService.searchValue = {}
-    this.functionalityService.pageIndex = 0
-    this.pageIndex = 0
-    this.createForm()
-    this.loadInitialData()
-  }
-
-  loadInitialData() {
-    if (JSON.stringify(this.functionalityService.searchValue) === JSON.stringify(this.formCopy)) {
-      this.loadFunctionalities({}, this.pageIndex + 1)
-    } else {
-      this.params = this.getParams(this.functionalityService.searchValue)
-      this.loadFunctionalities(this.params, this.functionalityService.pageIndex + 1)
-    }
-
-    this.updateFilterActive()
-  }
-
-  loadFunctionalities(params, page: number) {
-    this.searching = true
-    let snackBar = this.snackBar.open('Carregando dados...')
-
-    this.functionalityService.functionalities(params, page).subscribe(dataInfo => {
-      this.searching = false
-      this.dataInfo = dataInfo
-      this.pagination = dataInfo.pagination
-      this.functionalities = <Functionality[]> this.pagination.data
-      snackBar.dismiss()
-    })
-  }
-
-  delete(functionality: Functionality) {
-    this.functionalityService.delete(functionality.id).subscribe((data) => {
-      this.snackBar.open(data.message, '', {
-        duration: 5000
-      })
-
-      if(data.status) {
-        this.functionalities.splice(this.functionalities.indexOf(functionality), 1)
+    this.listData = {
+      header: {
+        filterFields: [
+          mF({
+            class: 'col-md-3',
+            formcontrolname: 'search',
+            placeholder: 'Descrição',
+            type: 'input'
+          }),
+        ],
+        getParams: (formValue) => {
+          return {
+            search: formValue.search
+          }
+        }
+      },
+      body: {
+        dataFields: [
+          {
+            label: 'ID',
+            style: { width: '12%' },
+            showData: (functionality: Functionality) => {
+              return functionality.id
+            }
+          },
+          {
+            label: 'Descrição',
+            style: { width: '30%' },
+            showData: (functionality: Functionality) => {
+              return functionality.description
+            }
+          },
+          {
+            label: 'URL',
+            style: { width: '50%' },
+            showData: (functionality: Functionality) => {
+              return functionality.url
+            }
+          },
+        ],
+        hasMenuButton: true,
+        menuItems: [
+          {
+            icon: 'subject',
+            label: 'Detalhes',
+            actions: {
+              click: (functionality: Functionality) => {
+                return this.router.navigate(['/cost-categories/show', functionality.id])
+              },
+              disabled: (functionality: Functionality) => {
+                return !this.permissionVerify('show', functionality)
+              }
+            }
+          },
+          {
+            icon: 'mode_edit',
+            label: 'Editar',
+            actions: {
+              click: (functionality: Functionality) => {
+                return this.router.navigate(['/cost-categories/edit', functionality.id])
+              },
+              disabled: (functionality: Functionality) => {
+                return !this.permissionVerify('edit', functionality)
+              }
+            }
+          },
+          {
+            icon: 'delete',
+            label: 'Remover',
+            removeWhenClickTrue: true,
+            actions: {
+              click: (functionality: Functionality) => {
+                return this.delete(functionality)
+              },
+              disabled: (functionality: Functionality) => {
+                return !this.permissionVerify('delete', functionality)
+              }
+            }
+          },
+        ],
+        loadData: (params, page) => {
+          return this.functionalityService.functionalities(params, page)
+        },
+        buttonStyle: { width: '8%' }
       }
-    })
+    }
   }
 
-  changePage($event) {
-    this.searching = true
-    this.functionalities = []
+  async delete(functionality: Functionality) {
+    let data = await this.functionalityService.delete(functionality.id).toPromise()
 
-    this.functionalityService.functionalities(this.functionalityService.searchValue, ($event.pageIndex + 1)).subscribe(dataInfo => {
-      this.searching = false
-      this.dataInfo = dataInfo
-      this.pagination = dataInfo.pagination
-      this.functionalities = <Functionality[]> this.pagination.data
-      this.pageIndex = $event.pageIndex
-      this.functionalityService.pageIndex = this.pageIndex
+    this.snackBar.open(data.message, '', {
+      duration: 5000
     })
-  }
 
+    return data.status
+  }
 }
