2fba79d9b3290ab66ede930cc4bc3e447a50ae65
elissandro <elissandros667@gmail.com>
2023-11-28 20:13:22 -0300

    - Correções dashboard

--- src/app/home/components/chart-preview.component.ts
+++ src/app/home/components/chart-preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material';
-import { ChartOptions } from '../home.component';
+import { ChartOptions, LineChartOptions } from '../home.component';
+import { HomeData } from '../models/home-data.model';
 
 @Component({
   selector: 'cb-chart-preview',
@@ -8,138 +9,105 @@ import { ChartOptions } from '../home.component';
   styleUrls: ['./chart-preview.component.css']
 })
 export class ChartPreviewComponent {
-  @Input() chartOptions: any;
+  @Input() homeData: HomeData;
   @Input() chartType: number;
   @Input() title: string;
   EChartType = EChartType;
-
-  chartOptionsDonut: Partial<ChartOptions> = {
-    series: [44, 55, 41, 17],
-    chart: {
-      width: 500,
-      type: "donut",
-    },
-    stroke: {
-      width: 0
-    },
-    legend: {
-      position: "left",
-      markers: {
-        radius: 0,
-        height: 10,
-
-      }
-    },
-    plotOptions: {
-      pie: {
-        donut: {
-          size: "80%",
-          labels: {
-            show: true,
-            total: {
-              showAlways: true,
-              show: true,
-              fontSize: "120px",
-              fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif",
-              color: "#57585a",
-              fontWeight: "300",
-              formatter: (a) => "1.200.000",
-              label: '103'
-            },
-            value: {
-              fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif",
-              color: "#57585a",
-              fontWeight: "100",
-              fontSize: "30px",
-              offsetY: 50,
-            },
-            name : {
-              offsetY: 20
-            }
-          }
-        }
-      }
-    },
-    labels: ["Cenografia", "Stand", "PDV", "Showrooms", 'Outsiders'],
-    colors: ["#adca5f", "#e82489", "#4fa2b1", "#00abeb", "#ffcd37"],
-    dataLabels: {
-      enabled: false
-    },
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200
-          },
-          legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
+  chartOptions:Partial<LineChartOptions> = {
+    series: [],
+    chart: { height: 270, type: "line", toolbar: { show: false }},
+    grid: { borderColor: "#fff", position: "back", xaxis: { lines: { show: true } }, yaxis: { lines: { show: true } } },
+    colors: [],
+    dataLabels: { enabled: false },
+    stroke: { curve: "straight", width: [2, 4] },
+    markers: { size: 1 },
+    xaxis: { categories: [] },
+    yaxis: { show: false },
+    legend: { show: false },
   };
 
+  chartOptionsPieJobs: Partial<ChartOptions> = {
+    series: [],
+    chart: { width: 500, type: "donut" },
+    stroke: { width: 0 },
+    legend: { position: "left", markers: { radius: 0, height: 10 } },
+    plotOptions: { pie: { donut: { size: "80%", labels: { show: true, total: { showAlways: true, show: true, fontSize: "80px", fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif", color: "#57585a", fontWeight: "300" }, value: { fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif", color: "#57585a", fontWeight: "100", fontSize: "20px", offsetY: 25 }, name : { offsetY: 10 } } } } },
+    labels: [],
+    dataLabels: { enabled: false },
+    colors: [],
+    responsive: [{ breakpoint: 480, options: { chart: { width: 200 }, legend: { position: "bottom" } } }],
+    tooltip: { y: {} }
+  };
 
-  chartOptionsLine = {
-    series: [
-      {
-        name: "High - 2013",
-        data: [28, 29, 33, 30, 45, 68, 68, 43, 42, 55, 33, 33],
-      },
-      {
-        name: "Low - 2013",
-        data: [12, 20, 25, 60, 32, 20, 10, 50, 25, 33, 33, 33]
-      }
-    ],
-    chart: {
-      height: 400,
-      width: 1000,
-      type: "line",
-      toolbar: {
-        show: false
-      }
-    },
-    grid: {
-      borderColor: "#fff", // Cor das bordas da grade
-      position: "back", // Coloca a grade atrás do gráfico
-      xaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      yaxis: {
-        lines: {
-          show: true,
-        },
-      },
-    },
-    colors: ["#77B6EA", "#545454"],
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "straight",
-      width: [2, 4]
-    },
-    markers: {
-      size: 1
-    },
-    xaxis: {
-      categories: ["Jan 23", "Fev 23", "Mar 23", "Abr 23", "Mai 23", "Jun 23", "Jul 23", "Ago 23", "Set 23", "Out 23", "Nov 23", "Dez 23"],
-    },
-    yaxis: {
-      show: false,
-    },
-    legend: {
-      show: false
-    }
+  chartOptionsPie: Partial<ChartOptions> = {
+    series: [],
+    chart: { width: 500, type: "donut" },
+    stroke: { width: 0 },
+    legend: { position: "left", markers: { radius: 0, height: 10 } },
+    plotOptions: { pie: { donut: { size: "80%", labels: { show: true, total: { showAlways: true, show: true, fontSize: "80px", fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif", color: "#57585a", fontWeight: "300" }, value: { fontFamily: "-apple-system,BlinkMacSystemFont, Segoe UI ,Roboto, Helvetica Neue,Arial,sans-serif", color: "#57585a", fontWeight: "100", fontSize: "20px", offsetY: 25 }, name : { offsetY: 10 } } } } },
+    labels: [],
+    colors: [],
+    dataLabels: { enabled: false },
+    responsive: [ { breakpoint: 480, options: { chart: { width: 200 }, legend: { position: "bottom" } } } ],
+    tooltip: { y: {} }
   };
 
   constructor(public dialog: MatDialog) { }
+
+  configureChartOptionsPieJobs() {
+    this.chartOptionsPieJobs.series = this.homeData.jobs.series;
+    this.chartOptionsPieJobs.plotOptions.pie.donut.labels.total.formatter = () => this.homeData.jobs.meta_jobs.toLocaleString(undefined, { minimumFractionDigits: 0 }),
+    this.chartOptionsPieJobs.plotOptions.pie.donut.labels.total.label = this.homeData.jobs.total.toLocaleString(undefined, { minimumFractionDigits: 0 });
+    this.chartOptionsPieJobs.labels = this.homeData.jobs.labels;
+    this.chartOptionsPieJobs.colors = this.homeData.jobs.colors;
+    this.chartOptionsPieJobs.tooltip = { y: { formatter: (val) => this.getValue(val) } }
+
+    if (this.homeData.jobs.total > 99999) {
+      this.chartOptionsPieJobs.plotOptions.pie.donut.labels.total.fontSize = "45px";
+    }
+  }
+
+  getValue(val: number) {
+    const jobTypes = ['aprovados', 'avancados', 'ajustes', 'stand_by', 'reprovados'];
+  
+    for (const type of jobTypes) {
+      if (this.homeData.jobs[type].total === val) {
+        return `${this.homeData.jobs[type].porcentagem}%`;
+      }
+    }
+  
+    return '0%';
+  }
+
+  configureChartOptionsPie() {
+    this.chartOptionsPie.series = this.homeData.jobs2.series.map(x => Number(x));
+    this.chartOptionsPie.tooltip = { y: { formatter: (val) => `${val}%` } };
+    this.chartOptionsPie.plotOptions.pie.donut.labels.total.formatter = () => this.homeData.jobs2.meta_jobs.toLocaleString(undefined, { minimumFractionDigits: 0 }),
+    this.chartOptionsPie.plotOptions.pie.donut.labels.total.label = this.homeData.jobs2.total.toLocaleString(undefined, { minimumFractionDigits: 0 });
+    this.chartOptionsPie.labels = this.homeData.jobs2.labels;
+    this.chartOptionsPie.colors = this.homeData.jobs2.colors;
+    
+
+    if (this.homeData.jobs2.total > 99999) {
+      this.chartOptionsPieJobs.plotOptions.pie.donut.labels.total.fontSize = "45px";
+    }
+  }
+
+  configureChartLine() {
+    this.chartOptions.series = this.homeData.tendencia.series;
+    this.chartOptions.colors = this.homeData.tendencia.colors;
+    this.chartOptions.xaxis.categories = this.homeData.tendencia.meses_ano;
+
+    this.chartOptions.tooltip = {
+      y: {
+          formatter: (val) => val.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+      },
+    };
+  }
 }
 
 export enum EChartType {
-  DONUT = 1,
-  LINE = 2
+  JOBS = 1,
+  JOBS2 = 2,
+  TENDENCIA = 3,
 }
