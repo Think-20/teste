c1150ea433d2cc7d4340b386ff68f845ae5f69ea
elissandro <elissandros667@gmail.com>
2024-02-20 18:58:54 -0300

    poc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // api: 'http://ec2-44-211-162-19.compute-1.amazonaws.com:8000'
-  api: 'http://100.26.59.205:8000'
+  api: 'http://184.72.87.177:8000'
 };
