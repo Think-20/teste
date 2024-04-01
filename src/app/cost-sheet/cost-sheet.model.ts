abb9ec8204536140cd85668be8686ca17d548dc1
elissandro <elissandros667@gmail.com>
2024-04-01 20:01:12 -0300

    - Ficha de custo

--- src/app/cost-sheet/cost-sheet.model.ts
+++ src/app/cost-sheet/cost-sheet.model.ts
@@ -29,17 +29,17 @@ export class CostSheetGroup {
     showFooter: boolean;
 
     constructor(
-        id: number | null = null,
-        titulo: string | null = null,
-        percentual: number | null = null,
-        valor: number | null = null,
-        cost_sheets: Partial<CostSheet>[] | null = null,
-        total_previsto: number | null = null,
-        valor_previsto_realizado_percentual_total: number | null = null,
-        valor_previsto_realizado_percentual_total_neagtive: boolean | null = null,
-        total_realizado: number | null = null,
-        negociacao: Negociacao | null = new Negociacao(),
-        aprovacao: Aprovacao | null = new Aprovacao()
+        id?: number,
+        titulo?: string,
+        percentual?: number,
+        valor?: number,
+        cost_sheets?: Partial<CostSheet>[],
+        total_previsto?: number,
+        valor_previsto_realizado_percentual_total?: number,
+        valor_previsto_realizado_percentual_total_neagtive?: boolean,
+        total_realizado?: number,
+        negociacao?: Negociacao,
+        aprovacao?: Aprovacao
     ) {
         this.id = id;
         this.titulo = titulo;
@@ -50,8 +50,8 @@ export class CostSheetGroup {
         this.valor_previsto_realizado_percentual_total = valor_previsto_realizado_percentual_total;
         this.valor_previsto_realizado_percentual_total_neagtive = valor_previsto_realizado_percentual_total_neagtive;
         this.total_realizado = total_realizado;
-        this.negociacao = negociacao;
-        this.aprovacao = aprovacao;
+        this.negociacao = negociacao || new Negociacao();
+        this.aprovacao = aprovacao || new Aprovacao();
         this.expanded = true;
         this.showFooter = true;
     }
@@ -78,43 +78,43 @@ export class CostSheet {
     pagamento: string[];
 
     constructor(
-        id: number | null = null,
-        numero: string | null = null,
-        categoria: string | null = null,
-        favorecido: Partial<Favorecido> | null = new Favorecido(),
-        descricao: string | null = null,
-        quantidade: number | null = null,
-        unidade: string | null = null,
-        valor_previsto: number | null = null,
-        valor_realizado: number | null = null,
-        valor_previsto_realizado_percentual: number | null = null,
-        negociacao: Partial<Negociacao> | null = new Negociacao(),
-        solicitante: Partial<Solicitante> | null = new Solicitante(),
-        aprovacao: Partial<Aprovacao> | null = new Aprovacao(),
-        aceite: Partial<Aceite> | null = new Aceite(),
-        nf: string | null = null,
-        condicao: string[] | null = null,
-        vencimento: Partial<Vencimento> | null = new Vencimento(),
-        pagamento: string[] | null = null
+        id?: number,
+        numero?: string,
+        categoria?: string,
+        favorecido?: Partial<Favorecido>,
+        descricao?: string,
+        quantidade?: number,
+        unidade?: string,
+        valor_previsto?: number,
+        valor_realizado?: number,
+        valor_previsto_realizado_percentual?: number,
+        negociacao?: Partial<Negociacao>,
+        solicitante?: Partial<Solicitante>,
+        aprovacao?: Partial<Aprovacao>,
+        aceite?: Partial<Aceite>,
+        nf?: string,
+        condicao?: string[],
+        vencimento?: Partial<Vencimento>,
+        pagamento?: string[]
     ) {
         this.id = id;
         this.numero = numero;
         this.categoria = categoria;
-        this.favorecido = favorecido;
+        this.favorecido = favorecido || new Favorecido();
         this.descricao = descricao;
         this.quantidade = quantidade;
         this.unidade = unidade;
         this.valor_previsto = valor_previsto;
         this.valor_realizado = valor_realizado;
         this.valor_previsto_realizado_percentual = valor_previsto_realizado_percentual;
-        this.negociacao = negociacao;
-        this.solicitante = solicitante;
-        this.aprovacao = aprovacao;
-        this.aceite = aceite;
+        this.negociacao = negociacao ||  new Negociacao();
+        this.solicitante = solicitante || new Solicitante();
+        this.aprovacao = aprovacao || new Aprovacao();
+        this.aceite = aceite || new Aceite();
         this.nf = nf;
-        this.condicao = condicao;
-        this.vencimento = vencimento;
-        this.pagamento = pagamento;
+        this.condicao = condicao || [];
+        this.vencimento = vencimento || new Vencimento();
+        this.pagamento = pagamento || [];
     }
 }
 
