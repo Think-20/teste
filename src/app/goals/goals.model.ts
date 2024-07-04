def9180b610d2f54713d72de99fac6494c1e2fca
Think <think_ideias@outlook.com>
2024-07-04 20:44:08 -0300

    - Metas novo layout

--- src/app/goals/goals.model.ts
+++ src/app/goals/goals.model.ts
@@ -8,7 +8,9 @@ export interface Goal {
     month_name: string 
     month: number,
     year: number,
-    value: number,
-    expected_value: number,
+    value_internal: number,
+    expected_value_external: number,
+    value_external: number,
+    expected_value_internal: number,
     message?: string;
 }
