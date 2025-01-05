a4101db47a5f772a98838149e865f8bf892e22fa
Brenno Henrique Santos Oliveira <brennohh22@hotmail.com>
2025-01-05 17:23:19 -0300

    Alterações DUO

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://54.163.167.198:8000' // HOMOLOGAÇÃO
+  api: 'http://52.207.117.25:8000' // HOMOLOGAÇÃO
   // api: 'http://107.20.217.33:8000' // PRODUÇÃO
 };
