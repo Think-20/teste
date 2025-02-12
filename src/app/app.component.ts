4c0460e4b5ffa599a9d4c8b52b0f7320cb6e213e
Brenno Henrique Santos Oliveira <brennohh22@hotmail.com>
2025-02-11 22:48:59 -0300

    Resolver problema de URL com excesso de informações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from "@angular/core";
 
 @Component({
-  selector: 'cb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "cb-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
+  
+  ngOnInit(): void {
+    const http = "http:";
+    const https = "https:";
+
+    if (isPlatformBrowser(this.platformId) && window.location.protocol.startsWith(https)) {
+      const newUrl = window.location.href.replace(https, http);
+
+      window.location.href = newUrl;
+    }
+  }
+}
